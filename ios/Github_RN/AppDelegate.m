3873b8c401789c3fdf9b106d3c300f881720d034
YjjTT <yjjtt@tempot.com>
2019-04-19 15:40:13 +0800

    打包

--- ios/Github_RN/AppDelegate.m
+++ ios/Github_RN/AppDelegate.m
@@ -32,11 +32,11 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-#if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-#else
+//#if DEBUG
+//  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+//#else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
+//#endif
 }
 
 @end
